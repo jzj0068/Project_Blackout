6d9518308d279e88adbb620851ebb740ff5eff37
Jacob T Stockwell <JacobTStockwell@gmail.com>
2021-03-16 15:47:29 -0500

    Agent and background basics
    In this commit:
    - Added basic movement in AgentMovement.cs
    - Modified pixel art graphics settings

---
+++ TeamProj_Blackout/Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 
+            Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position = transform.position + movement * Time.deltaTime * speed;
+        
+        
+    }
+}
