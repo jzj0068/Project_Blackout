c1c2df5e75a2f3d656276200253707a9958d97ff
Jacob T Stockwell <JacobTStockwell@gmail.com>
2021-03-18 17:20:30 -0500

    Animation and Skeleton game level
    in this commit:
    
    - got animation working for moving left and right
    - Skeleton layout of the game

--- TeamProj_Blackout/Assets/Scripts/AgentMovement.cs
+++ TeamProj_Blackout/Assets/Scripts/AgentMovement.cs
@@ -6,20 +6,26 @@ public class AgentMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 5.0f;
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+    
+
 
     // Update is called once per frame
     void Update()
     {
+
+       
+
+        
+         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+         //animator2.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
 
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 
             Input.GetAxis("Vertical"), 0.0f);
 
         transform.position = transform.position + movement * Time.deltaTime * speed;
         
-        
+
     }
 }
