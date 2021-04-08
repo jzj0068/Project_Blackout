8ea6dad59bf2d3099239e56b819d4bb25fdf15d1
jzj0068 <jzj0068@auburn.edu>
2021-04-08 13:22:49 -0500

    ANNNNND SCENE

---
+++ blackout/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float waittime = 2f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    LoadNextLevel();
+        //}
+    }
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
+    void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //play animation
+        transition.SetTrigger("Start");
+        //wait
+        yield return new WaitForSeconds(waittime);
+
+        //load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
